675eb98282349263c33373eebbd33d479f91b269
edwinnambaje <edwinnambaje4@gmail.com>
2024-02-02 23:37:44 +0200

    updating backend env

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 export class Constants {
   static NODE_BACKEND_URL: string =
     import.meta.env.NODE_BACKEND_URL ||
-    "http://localhost:7002/api/v1/";
+    "https://task-trackers.onrender.com/api/v1/";
 
   static DEFAULT_ERROR_MESSAGE: string =
     "Something went wrong, please try again later.";
